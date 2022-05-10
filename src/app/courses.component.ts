41a0bff765e0b37ac58ec87fed10c1aea8ef866a
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-10 12:17:12 +0530

    attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,19 +1,12 @@
 import { Component } from "@angular/core";
-import { CoursesService } from "./courses.service";
 
 @Component({
     selector: 'courses',
-    template: `
-        <h2>{{ title }}</h2>
-        <h2 [textContent]="title"></h2> 
-        <img src="{{ imageUrl }}" />
-        <img [src]="imageUrl" /> 
-    `,
+    templateUrl: './courses.component.html',
 })
 export class CoursesComponent {
 
-    title = "List of Courses";
-    // imageUrl = "http://lorempixel.com/400/200";
     imageUrl = "https://seewhatgrows.org/wp-content/uploads/2014/02/lorempixel.jpg";
+    colSpan = 2;
 
 } 
