1cebebaae37a9c213e3e532a3010e9994b96c722
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-11 17:04:48 +0530

    validatingh form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
 
-  constructor() { }
+  log(x: NgModel) {
+    console.log(x);
 
-  ngOnInit(): void {
   }
 
 }
