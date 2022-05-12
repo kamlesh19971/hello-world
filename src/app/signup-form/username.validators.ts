5529cedc9cae579123d1508089819d778b9497d1
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-12 11:50:46 +0530

    signup-form async operation added

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -2,11 +2,26 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export class UserNamValidators {
 
+    static users: string[] = [];
+
     static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
 
         if ((control.value as string).indexOf(' ') >= 0)
             return { cannotContainSpace: true };
 
+        return null;
+    }
+
+    static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+
+        setTimeout(() => {
+            console.log('ok');
+            if (this.users.findIndex(x => x == control.value) != -1) {
+                return { shouldBeUnique: true }
+            }
+            return null;
+
+        }, 2000);
         return null;
     }
 }
