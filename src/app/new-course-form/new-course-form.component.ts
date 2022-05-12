3662cceb85643e99f225959108492b65cd0062dd
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-12 15:44:24 +0530

    formbuilder added

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -8,21 +8,25 @@ import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/for
 })
 export class NewCourseFormComponent {
 
-  form = new FormGroup({
-    topics: new FormArray([])
-  });
+  form;
+  // new FormGroup({
+  //   name: new FormControl('', Validators.required),
+  //   contact: new FormGroup({
+  //     email: new FormControl(),
+  //     phone: new FormControl()
+  //   }),
+  //   topics: new FormArray([])
+  // });
 
-  addTopic(topic: HTMLInputElement) {
-    this.topics.push(new FormControl(topic.value));
-    topic.value = "";
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
   }
 
-  get topics() {
-    return this.form.get('topics') as FormArray;
-  }
-
-  removeTopic(topic: AbstractControl) {
-    let index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
-  }
 }
