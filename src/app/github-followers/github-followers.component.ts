6401694f06ae7af2b050e867bb0905d989bedd46
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-16 12:29:55 +0530

    optional parameter

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../services/github-followers.service';
 
 @Component({
@@ -10,10 +11,20 @@ export class GithubFollowersComponent implements OnInit {
 
   followers: any[] = [];
 
-  constructor(private service: GithubFollowersService) {
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) {
   }
 
   ngOnInit(): void {
+
+    this.route.paramMap.subscribe();
+    // let id = this.route.snapshot.paramMap.get('id');
+
+
+    this.route.queryParamMap.subscribe();
+    // let page = this.route.snapshot.queryParamMap.get('page');
+
 
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
