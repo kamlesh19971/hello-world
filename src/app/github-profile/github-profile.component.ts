21852a93713b11e60a04d38afef38b28b8c13345
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-16 12:17:34 +0530

    Obsevable

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,12 +11,21 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // snapshot based
+    let id = this.route.snapshot.paramMap.get("id");
+    console.log(id);
+
+
+    // Subscription
     this.route.paramMap
       .subscribe(params => {
         let id = params.get("id");
-        console.log(id ? +id : id);
+        console.log(id);
 
       });
+
   }
+
 
 }
