79b9dee18da15567484a1b570ace86686f522a20
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-16 12:39:48 +0530

    combine multiple observables

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../services/github-followers.service';
+import { Observable } from 'rxjs';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'github-followers',
@@ -18,16 +20,19 @@ export class GithubFollowersComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.route.paramMap.subscribe();
-    // let id = this.route.snapshot.paramMap.get('id');
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ]).subscribe(combined => {
 
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
 
-    this.route.queryParamMap.subscribe();
-    // let page = this.route.snapshot.queryParamMap.get('page');
+      // this.service.getAll({ id: id, page: page });
 
-
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers);
+      this.service.getAll()
+        .subscribe(followers => this.followers = followers);
+    });
   }
 
 }
